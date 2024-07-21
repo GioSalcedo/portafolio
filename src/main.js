c6a5b59f3a9588e078821303211d9497a57591d1
Giovanny Salcedo <giovanny.salcedo@genstudents.org>
2024-07-20 21:06:37 -0500

    fix: ico

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { router } from "./routes/routes";
 import "./main.scss";
-import iconPortafolio from './images/icon-portafolio.ico';
 
 window.addEventListener("hashchange", () => {
   router(window.location.hash);
